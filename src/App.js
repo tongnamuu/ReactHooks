cab3ebb9c147c79c98d76ebb8d61b649fc32de99
tongnamuu <thban33@naver.com>
2021-05-21 23:34:45 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import "./App.css";
 import React, { useState } from "react";
 import useInput from "./Hooks/useInput";
 import useTabs from "./Hooks/useTabs";
+import useTitle from "./Hooks/useTitle";
+import useClick from "./Hooks/useClick";
+import useConfirm from "./Hooks/useConfirm";
+import { usePreventLeave } from "./Hooks/usePreventLeave";
+import { useBeforeLeave } from "./Hooks/useBeforeLeave";
+import { useFadeIn } from "./Hooks/useFadeIn";
+import { useNetwork } from "./Hooks/useNetwork";
 
 const content = [
   {
@@ -24,6 +31,24 @@ function App() {
 
   const { currentItem, changeItem } = useTabs(0, content);
 
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+
+  const sayHello = () => console.log("say hello");
+  const contentTitle = useClick(sayHello);
+
+  const deleteWorld = () => console.log("Deleting the World...");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
+
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+
+  useBeforeLeave(() => console.log("Are you Leaving?"));
+
+  const fadeInElement1 = useFadeIn(1, 2);
+  const fadeInElement2 = useFadeIn(5, 3);
+
+  const onLine = useNetwork();
   return (
     <>
       <div>
@@ -43,6 +68,23 @@ function App() {
         ))}
         <div>{currentItem.content}</div>
       </div>
+      <div className="useClick">
+        <h1 ref={contentTitle}>Hello if Click here</h1>
+      </div>
+      <div className="useConfirm">
+        <button onClick={confirmDelete}>Delete the World!</button>
+      </div>
+      <div className="usePreventLeave">
+        <button onClick={enablePrevent}>Protect</button>
+        <button onClick={disablePrevent}>UnProtect</button>
+      </div>
+      <div className="useFadeIn1" {...fadeInElement1}>
+        useFadeIn 1
+      </div>
+      <div className="useFadeIn2" {...fadeInElement2}>
+        useFadeIn 2
+      </div>
+      <div className="useNetwork">{onLine ? "Online" : "Offline"}</div>
     </>
   );
 }

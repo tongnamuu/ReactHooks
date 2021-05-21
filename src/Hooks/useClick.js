b3c59517c9cadb4c4ead51d5602929ee9f2d8451
tongnamuu <thban33@naver.com>
2021-05-21 23:35:02 +0900

    Add useClick

---
+++ src/Hooks/useClick.js
@@ -0,0 +1,19 @@
+import { useRef, useEffect } from "react";
+
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+
+export default useClick;
